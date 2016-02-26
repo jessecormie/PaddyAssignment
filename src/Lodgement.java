4c51c01425bfef34b37721b7bb9bb2cef6b3108f
jessecormie <C12357466@mydit.ie>
2016-02-26 18:56:26 +0000

    Fix lodgement by getting rid of booleans and creating another method

--- src/Lodgement.java
+++ src/Lodgement.java
@@ -6,71 +6,59 @@ import javax.swing.JOptionPane;
 public class Lodgement {
 
 	private JFrame f;
+	int count = 3;
+	private CustomerAccount acc;
+	private double balance;
 
 	public void lodgement(CustomerAccount acc) {
-		boolean loop = true;
-		boolean on = true;
-		double balance = 0;
-
+		this.acc = acc;
 		if (acc instanceof CustomerCurrentAccount) {
-			int count = 3;
 			int checkPin = ((CustomerCurrentAccount) acc).getAtm().getPin();
-			loop = true;
-
-			while (loop) {
-				if (count == 0) {
-					JOptionPane.showMessageDialog(f, "Pin entered incorrectly 3 times. ATM card locked.", "Pin",
-							JOptionPane.INFORMATION_MESSAGE);
-					((CustomerCurrentAccount) acc).getAtm().setValid(false);
-					loop = false;
-					on = false;
-				}
-
+			if (count == 0) {
+				JOptionPane.showMessageDialog(f, "Pin entered incorrectly 3 times. ATM card locked.", "Pin",
+						JOptionPane.INFORMATION_MESSAGE);
+				((CustomerCurrentAccount) acc).getAtm().setValid(false);
+			} else {
 				String Pin = JOptionPane.showInputDialog(f, "Enter 4 digit PIN;");
 				int i = Integer.parseInt(Pin);
-
-				if (on) {
-					if (checkPin == i) {
-						loop = false;
-						JOptionPane.showMessageDialog(f, "Pin entry successful", "Pin",
-								JOptionPane.INFORMATION_MESSAGE);
-
-					} else {
-						count--;
-						JOptionPane.showMessageDialog(f, "Incorrect pin. " + count + " attempts remaining.", "Pin",
-								JOptionPane.INFORMATION_MESSAGE);
-					}
-
+				if (checkPin == i) {
+					JOptionPane.showMessageDialog(f, "Pin entry successful", "Pin", JOptionPane.INFORMATION_MESSAGE);
+					lodgeMoney();
+				} else {
+					count--;
+					JOptionPane.showMessageDialog(f, "Incorrect pin. " + count + " attempts remaining.", "Pin",
+							JOptionPane.INFORMATION_MESSAGE);
+					lodgement(acc);
 				}
 			}
-
+		} else {
+			lodgeMoney();
 		}
-		if (on == true) {
-			String balanceTest = JOptionPane.showInputDialog(f, "Enter amount you wish to lodge:");
-			if (Menu.isNumeric(balanceTest)) {
+	}
 
-				balance = Double.parseDouble(balanceTest);
-				loop = false;
+	public void lodgeMoney() {
+		String balanceTest = JOptionPane.showInputDialog(f, "Enter amount you wish to lodge:");
+		if (Menu.isNumeric(balanceTest)) {
+			balance = Double.parseDouble(balanceTest);
 
-			} else {
-				JOptionPane.showMessageDialog(f, "You must enter a numerical value!", "Oops!",
-						JOptionPane.INFORMATION_MESSAGE);
-			}
-
-			String euro = "\u20ac";
-			acc.setBalance(acc.getBalance() + balance);
-			Date date = new Date();
-			String date2 = date.toString();
-			String type = "Lodgement";
-			double amount = balance;
-
-			AccountTransaction transaction = new AccountTransaction(date2, type, amount);
-			acc.getTransactionList().add(transaction);
-
-			JOptionPane.showMessageDialog(f, balance + euro + " added do you account!", "Lodgement",
-					JOptionPane.INFORMATION_MESSAGE);
-			JOptionPane.showMessageDialog(f, "New balance = " + acc.getBalance() + euro, "Lodgement",
+		} else {
+			JOptionPane.showMessageDialog(f, "You must enter a numerical value!", "Oops!",
 					JOptionPane.INFORMATION_MESSAGE);
 		}
+
+		String euro = "\u20ac";
+		acc.setBalance(acc.getBalance() + balance);
+		Date date = new Date();
+		String date2 = date.toString();
+		String type = "Lodgement";
+		double amount = balance;
+
+		AccountTransaction transaction = new AccountTransaction(date2, type, amount);
+		acc.getTransactionList().add(transaction);
+
+		JOptionPane.showMessageDialog(f, balance + euro + " added do you account!", "Lodgement",
+				JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(f, "New balance = " + acc.getBalance() + euro, "Lodgement",
+				JOptionPane.INFORMATION_MESSAGE);
 	}
 }
